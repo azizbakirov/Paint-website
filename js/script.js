99b8f3657f61cca9a39338f2617211cc77b139d7
Aziz Bakirov <azizbakirov9@gmail.com>
2022-12-23 04:17:53 +0500

    succes color

--- js/script.js
+++ js/script.js
@@ -1,30 +1,134 @@
-const canvas = document.querySelector("canvas");
+// GLOBALS VARIABLES
+const canvas = document.querySelector("canvas"),
+  toolBtns = document.querySelectorAll(".tool"),
+  fillColor = document.querySelector("#fill-color"),
+  sizeSlide = document.querySelector("#size-slider"),
+  colorBtns = document.querySelectorAll(".colors .option"),
+  colorPicker = document.querySelector("#color-picker");
 
+// VARIABLES
 let ctx = canvas.getContext("2d"),
   isDrawing = false,
-  brushWidth = 5;
+  brushWidth = 5,
+  selectedTool = "brush",
+  selectedColor = "#000",
+  prewMouseX,
+  prewMouseY,
+  snapshot;
 
+// CANVAS VARIABLES
 window.addEventListener("load", () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 });
 
-const startDraw = () => {
+// START DRAWING
+const startDraw = (e) => {
   isDrawing = true;
+  prewMouseX = e.offsetX;
+  prewMouseY = e.offsetY;
   ctx.beginPath();
   ctx.lineWidth = brushWidth;
+  ctx.strokeStyle = selectedColor;
+  ctx.fillStyle = selectedColor;
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 };
 
+// START DRAWRECTANGLE
+const drawRectangle = (e) => {
+  fillColor.checked
+    ? ctx.fillRect(
+        e.offsetX,
+        e.offsetY,
+        prewMouseX - e.offsetX,
+        prewMouseY - e.offsetY,
+      )
+    : ctx.strokeRect(
+        e.offsetX,
+        e.offsetY,
+        prewMouseX - e.offsetX,
+        prewMouseY - e.offsetY,
+      );
+};
+
+// DRAW CIRCLE
+const drawCircle = (e) => {
+  ctx.beginPath();
+  const radius =
+    Math.sqrt(Math.pow(prewMouseX - e.offsetX, 2)) +
+    Math.pow(prewMouseY - e.offsetY, 2);
+  ctx.arc(prewMouseX, prewMouseY, radius, 0, 2 * Math.PI);
+  fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+// DRAW TRIANGLE
+const drawTriangle = (e) => {
+  ctx.beginPath();
+  ctx.moveTo(prewMouseX, prewMouseY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.lineTo(prewMouseX * 2 - e.offsetX, e.offsetY);
+  ctx.closePath();
+  fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+// START DRAWING
 const drawing = (e) => {
   if (!isDrawing) return;
-
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
+  ctx.putImageData(snapshot, 0, 0);
+  switch (selectedTool) {
+    case "brush":
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+      break;
+    case "rectangle":
+      drawRectangle(e);
+      break;
+    case "circle":
+      drawCircle(e);
+      break;
+    case "triangle":
+      drawTriangle(e);
+      break;
+    default:
+      break;
+  }
 };
+
+// CHANGE BRUSH WIDTH
+sizeSlide.addEventListener("change", () => (brushWidth = sizeSlide.value));
+
+// TOOLS BTNS
+toolBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    selectedTool = btn.id;
+    document.querySelector(".options .active").classList.remove("active");
+    btn.classList.add("active");
+    console.log(`selected ${selectedTool}`);
+  });
+});
 const stopDraw = () => {
   isDrawing = false;
 };
 
+// COLOR BTNS
+colorBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    document.querySelector(".options .selected").classList.remove("selected");
+    btn.classList.add("selected");
+    const bgColor = window
+      .getComputedStyle(btn)
+      .getPropertyValue("background-color");
+    selectedColor = bgColor;
+  });
+});
+
+// SET COLOR FROM PICKER
+colorPicker.addEventListener('change', ()=>{
+  colorPicker.parentElement.style.background = colorPicker.value
+  colorPicker.parentElement.click()
+})
+
+// DRAW EVENTS
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mousemove", drawing);
 canvas.addEventListener("mouseup", stopDraw);
